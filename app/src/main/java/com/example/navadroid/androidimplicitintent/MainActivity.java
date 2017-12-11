587ed67b88cd84cdb687cbf18dfcd965dac05c94
Bhurivat Montri <auntafung@hotmail.com>
2017-12-11 16:59:44 +0700

    570610594

--- app/src/main/java/com/example/navadroid/androidimplicitintent/MainActivity.java
+++ app/src/main/java/com/example/navadroid/androidimplicitintent/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.navadroid.androidimplicitintent;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -38,12 +39,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             case R.id.btn_intent_2:
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:0856053794"));
+                startActivity(intent);
                 // TODO: ???
                 break;
             case R.id.btn_intent_3:
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:47.6,-122.3"));
+                startActivity(intent);
                 // TODO: ???
                 break;
             case R.id.btn_intent_4:
+                intent.setAction(AlarmClock.ACTION_SET_ALARM);
+                intent.putExtra(AlarmClock.EXTRA_MESSAGE, "hello")
+                    .putExtra(AlarmClock.EXTRA_HOUR, 5)
+                    .putExtra(AlarmClock.EXTRA_MINUTES, 10);
+                startActivity(intent);
                 // TODO: ???
                 break;
             case R.id.btn_intent_5:
